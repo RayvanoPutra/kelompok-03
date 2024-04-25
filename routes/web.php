7e9098343fce5a3f1e2872d9103432f2faa15e64
Fredy <lilincemerlang@gmail.com>
2024-04-25 15:09:19 +0700

    menambah file yang diperlukan

--- routes/web.php
+++ routes/web.php
@@ -1,26 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\DashboardController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-Route::get('/login', function () {
-    return view('login');
+Route::get('/', function () {
+    return view('welcome');
 });
 
-Route::get('/registrasi', function () {
-    return view('regitrasi');
-});
+Route::get('/login', [LoginController::class, 'index']);
+Route::post('/login', [LoginController::class, 'authenticate']);
+
+Route::post('/dashboard', [DashboardController::class, 'index']);
